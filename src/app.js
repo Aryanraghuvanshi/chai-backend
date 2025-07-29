12ba81b02afce5d0f44a3afb3155a29371503ca4
Aryan Raghuvanshi <aryanraghuvanshi91@gmail.com>
2025-07-29 21:13:15 +0530

    added video like controller and created its routes .

--- src/app.js
+++ src/app.js
@@ -18,9 +18,11 @@ app.use(cookieParser())
 
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
+import likeRouter from './routes/like.routes.js'
 
 // routes declaration
 app.use("/api/v1/users" , userRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/like", likeRouter);
 
 export { app }
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,9 +4,42 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
+// Controller to like or unlike a video
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+
+    // 1. Validate videoId format
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400,"Invalid video Id");
+    }
+
+    // 2. Check if the video is already liked by this user
+    const existingLike = await Like.findOne({
+        video:videoId,
+        likedBy:req.user._id,
+    });
+    console.log(existingLike);
+    
+     // 3. If already liked, remove the like (unlike the video)
+    if(existingLike){
+        await existingLike.deleteOne();
+
+        return res.status(200).json(
+        new ApiResponse(200, { isLiked: false }, "Video unliked successfully")
+    );
+    }
+
+     // 4. If not liked yet, create a new like
+     await Like.create({
+        video:videoId,
+        likedBy:req.user._id,
+     });
+
+     
+  return res.status(200).json(
+    new ApiResponse(200, { isLiked: true }, "Video liked successfully")
+  );
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
